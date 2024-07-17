0b99bafe1bcb85174fa574aee9f27bc363a418e7
Shadot Hossan Riyad <103817482+shadot-Riyad@users.noreply.github.com>
2024-07-17 11:27:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function realtimeClock() {
+    var rtClock = new Date();
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
+
+    // Add AM and PM system
+    var amPm = ( hours < 12 ) ? "AM" : "PM";
+
+    // convert the hours component to 12 hour format
+    hours = (hours > 12) ? hours - 12 : hours;
+    
+    //pad the hours, minutes and seconds with leading pros
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    //Display the clock
+    document.getElementById('clock').innerHTML =
+    hours + " : " + minutes + " : " + seconds + " " + amPm;
+    var t = setTimeout(realtimeClock, 500);
+
+
+}
